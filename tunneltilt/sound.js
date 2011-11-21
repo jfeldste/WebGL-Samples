b2cbcd8b137896709d78b873557cb394ce39ecf9
Jonathan Feldstein <jfeldstein@rim.com>
2011-11-21 16:56:07 -0500

    Fixed an error with previous commit.

--- tunneltilt/sound.js
+++ tunneltilt/sound.js
@@ -12,10 +12,10 @@ function playSound(file, loops, volume, pan) {
         // Are we running in WebWorks?
         if (typeof blackberry != 'undefined') {
             blackberry.custom.audio.playFile(file, loops, volume, pan);
-        } else if (Audio != undefined) {
+        } else if (typeof Audio != undefined) {
             playSoundHTML5(file, loops, volume);
         }
-    } else if (Audio != undefined) {
+    } else if (typeof Audio != undefined) {
         playSoundHTML5(file, lioops, volume);
     }
 }
